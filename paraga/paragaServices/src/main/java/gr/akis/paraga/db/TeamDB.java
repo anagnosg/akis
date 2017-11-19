8bd29302a30e706ab18f1a9138ddd92bdf52cfb2
Roinas1985 <roinas_akindynos@yahoo.com>
2017-11-19 17:09:08 +0200

    jquery ajax post  insert  example. Περναμε data με ενα post jquery request στο server και αποθηκευονται στην βαση.

--- paraga/paragaServices/src/main/java/gr/akis/paraga/db/TeamDB.java
+++ paraga/paragaServices/src/main/java/gr/akis/paraga/db/TeamDB.java
@@ -1,8 +1,10 @@
 package gr.akis.paraga.db;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,6 +54,10 @@ import gr.anagnosg.employeeservices.db.utils.ConnectionWrapper;
 			}
 			return teams;
 		}	
+		
+		
+		
+		
 	
 	}
 
