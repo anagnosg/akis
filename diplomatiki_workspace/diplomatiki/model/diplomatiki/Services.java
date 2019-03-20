795f925b7834181d5d8b02592c1e498673bacb19
akis <roinas_akindynos@yahoo.com>
2019-03-20 18:43:30 +0200

    diplomatiki add workspace

---
+++ diplomatiki_workspace/diplomatiki/model/diplomatiki/Services.java
@@ -0,0 +1,66 @@
+package diplomatiki;
+
+public class Services {
+
+	Integer id;
+	Boolean disability_platform;
+	Boolean wc;
+	Boolean parkin;
+	String  offer;
+	Boolean system_braile;
+	Boolean deaf_language;
+	String  others;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Boolean getDisability_platform() {
+		return disability_platform;
+	}
+	public void setDisability_platform(Boolean disability_platform) {
+		this.disability_platform = disability_platform;
+	}
+	public Boolean getWc() {
+		return wc;
+	}
+	public void setWc(Boolean wc) {
+		this.wc = wc;
+	}
+	public Boolean getParkin() {
+		return parkin;
+	}
+	public void setParkin(Boolean parkin) {
+		this.parkin = parkin;
+	}
+	public String getOffer() {
+		return offer;
+	}
+	public void setOffer(String offer) {
+		this.offer = offer;
+	}
+	public Boolean getSystem_braile() {
+		return system_braile;
+	}
+	public void setSystem_braile(Boolean system_braile) {
+		this.system_braile = system_braile;
+	}
+	public Boolean getDeaf_language() {
+		return deaf_language;
+	}
+	public void setDeaf_language(Boolean deaf_language) {
+		this.deaf_language = deaf_language;
+	}
+	public String getOthers() {
+		return others;
+	}
+	public void setOthers(String others) {
+		this.others = others;
+	}
+	
+	
+
+	}
+
+
---
+++ diplomatiki_workspace/diplomatiki/src/diplomatiki/Login.java
@@ -0,0 +1,40 @@
+package diplomatiki;
+
+import java.util.Scanner;
+
+public class Login {
+
+
+		public static void main(String[] args) {
+		    Scanner input = new Scanner(System.in);
+ 
+		    String Username;
+		    String Password;
+
+		    Password = "123";
+		    Username = "win";
+
+		    Scanner input1 = new Scanner(System.in);
+		    System.out.println("Enter Username : ");
+		    String username = input1.next();
+
+		    Scanner input2 = new Scanner(System.in);
+		    System.out.println("Enter Password : ");
+		    String password = input2.next();
+		    
+		    if (username.equals(Username) && password.equals(Password)) {
+
+		        System.out.println("Access Granted! Welcome!");
+		    }
+
+		    else if (username.equals(Username)) {
+		        System.out.println("Invalid Password!");
+		    } else if (password.equals(Password)) {
+		        System.out.println("Invalid Username!");
+		    } else {
+		        System.out.println("Invalid Username & Password!");
+		    }
+
+		}
+}
+
