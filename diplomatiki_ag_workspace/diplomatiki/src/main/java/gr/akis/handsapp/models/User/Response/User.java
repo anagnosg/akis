73d7a908c5e2bb9273933c5c8f0208d8792d9ead
ganagnostopoulos <ganagnostopoulos@workloud.com>
2019-04-07 11:17:00 +0300

    Add properties to user model

--- diplomatiki_ag_workspace/diplomatiki/src/main/java/gr/akis/handsapp/models/User/Response/User.java
+++ diplomatiki_ag_workspace/diplomatiki/src/main/java/gr/akis/handsapp/models/User/Response/User.java
@@ -2,8 +2,13 @@ package gr.akis.handsapp.models.User.Response;
 
 import gr.akis.handsapp.models.User.Requests.CreateUserRequest;
 
-public class User extends CreateUserRequest{
+public class User {
 	Integer id;
+	String username;
+	String password;
+	int age;
+	int regionId;
+	String email;
 
 	public Integer getId() {
 		return id;
@@ -11,5 +16,45 @@ public class User extends CreateUserRequest{
 
 	public void setId(Integer id) {
 		this.id = id;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getRegionId() {
+		return regionId;
+	}
+
+	public void setRegionId(int regionId) {
+		this.regionId = regionId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 }
