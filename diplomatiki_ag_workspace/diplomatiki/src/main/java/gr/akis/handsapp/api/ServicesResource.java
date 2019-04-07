fbe2d4f56a00aee53953b29d1ec58637a2747f31
ganagnostopoulos <ganagnostopoulos@workloud.com>
2019-04-07 13:17:50 +0300

    Format code

--- diplomatiki_ag_workspace/diplomatiki/src/main/java/gr/akis/handsapp/api/ServicesResource.java
+++ diplomatiki_ag_workspace/diplomatiki/src/main/java/gr/akis/handsapp/api/ServicesResource.java
@@ -2,7 +2,6 @@ package gr.akis.handsapp.api;
 
 import java.util.List;
 import java.util.logging.Logger;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -11,7 +10,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-
 import gr.akis.handsapp.business.ServicesBs;
 import gr.akis.handsapp.models.ResponseModel;
 import gr.akis.handsapp.models.Services;
@@ -25,36 +23,31 @@ import io.swagger.annotations.Api;
 @Produces("application/json")
 public class ServicesResource {
 
- 	private static final Logger LOG = Logger.getLogger(ServicesResource.class.getName()); //Logger xrhsimopoieitai gia na katagraefis ston log tou application server tis energeies pou ginontai 
+	private static final Logger LOG = Logger.getLogger(ServicesResource.class.getName());
 
- 	@Inject
- 	ServicesBs serviceBS;
- 	@Inject
- 	GsonUtils gsUtils; // GsonUtils gsUtils = new GsonUtils(); 
-  
- 	@GET  //AUTA pou ksekinane me @ einai annotations 
- 	@Path("/all")  //Auta eiai annotations methodou. Mpanoun prin apo ton orismo ths methodou
- 	@Consumes("application/json")
- 	@Produces("application/json")
- 	public Response all() {
- 		
- 		//�� E ��� response Model ����� ��� ����� ��� student , List<Student>
- 		ResponseModel<List<Services>> rep = new ResponseModel<List<Services>>(); // Orismos antikeimenou rep
- 		try {  
- 			List<Services> listservice = serviceBS.selectAll();
- 			//sto antikeimeno rep. 8etoume ta data tou, me thn lista apo mathites.
- 			rep.setData(listservice);
- 			LOG.info("End all");
- 			return Response.ok(rep).build();
+	@Inject
+	ServicesBs serviceBS;
+	@Inject
+	GsonUtils gsUtils; // GsonUtils gsUtils = new GsonUtils();
 
- 		} catch (Exception e) {
- 			e.printStackTrace();
- 			rep.setError(e.getMessage(), Status.INTERNAL_SERVER_ERROR.getStatusCode());
- 			return Response.ok(rep).build();
- 		}
- 	}
+	@GET
+	@Path("/all")
+	@Consumes("application/json")
+	@Produces("application/json")
+	public Response all() {
 
+		ResponseModel<List<Services>> rep = new ResponseModel<List<Services>>(); // Orismos antikeimenou rep
+		try {
+			List<Services> listservice = serviceBS.selectAll();
+			rep.setData(listservice);
+			LOG.info("End all");
+			return Response.ok(rep).build();
 
+		} catch (Exception e) {
+			e.printStackTrace();
+			rep.setError(e.getMessage(), Status.INTERNAL_SERVER_ERROR.getStatusCode());
+			return Response.ok(rep).build();
+		}
 	}
 
-
+}
